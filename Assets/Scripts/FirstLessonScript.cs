ac4056a45390cebee50ae8aa3249d996f89d249f
yasindemirden <yasindemirden@hotmail.com>
2021-11-07 13:26:39 +0300

    * created: first lesson script

---
+++ Assets/Scripts/FirstLessonScript.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace LessonsBasic
+{
+    public class FirstLessonScript : MonoBehaviour
+    {
+        public int tamSayi = 1;
+        [SerializeField] int tamSayi2 = 100;
+        float ondalikSayi = 0.25f;
+        bool dogruYanlis = true;
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
